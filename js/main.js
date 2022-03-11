3e73d4440effb34a66841c52d84bd0566f0c1708
rahulputhukkot <rahulputhukkot@gmail.com>
2022-03-11 09:10:47 +0000

    particleJS implemetation phase1

---
+++ js/main.js
@@ -0,0 +1,396 @@
+/* ===================================================================
+ * Ceevee 2.0.0 - Main JS
+ *
+ * ------------------------------------------------------------------- */
+
+(function (html) {
+  "use strict";
+  html.className = html.className.replace(/\bno-js\b/g, "") + " js ";
+
+  /* Preloader
+   * -------------------------------------------------- */
+  const ssPreloader = function () {
+    const preloader = document.querySelector("#preloader");
+    if (!preloader) return;
+
+    window.addEventListener("load", function () {
+      document.querySelector("body").classList.remove("ss-preload");
+      document.querySelector("body").classList.add("ss-loaded");
+
+      preloader.addEventListener("transitionend", function (e) {
+        if (e.target.matches("#preloader")) {
+          this.style.display = "none";
+        }
+      });
+    });
+
+    // force page scroll position to top at page refresh
+    // window.addEventListener('beforeunload' , function () {
+    //     window.scrollTo(0, 0);
+    // });
+  }; // end ssPreloader
+
+//   ParticleJS
+//   -----------------------------------------------------
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#b61924",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}
+
+);
+
+  /* Parallax
+   * -------------------------------------------------- */
+  const ssParallax = function () {
+    const rellax = new Rellax(".rellax");
+  }; // end ssParallax
+
+  /* Move header menu
+   * -------------------------------------------------- */
+  const ssMoveHeader = function () {
+    const hdr = document.querySelector(".s-header");
+    const hero = document.querySelector("#hero");
+    let triggerHeight;
+
+    if (!(hdr && hero)) return;
+
+    setTimeout(function () {
+      triggerHeight = hero.offsetHeight - 170;
+    }, 300);
+
+    window.addEventListener("scroll", function () {
+      let loc = window.scrollY;
+
+      if (loc > triggerHeight) {
+        hdr.classList.add("sticky");
+      } else {
+        hdr.classList.remove("sticky");
+      }
+
+      if (loc > triggerHeight + 20) {
+        hdr.classList.add("offset");
+      } else {
+        hdr.classList.remove("offset");
+      }
+
+      if (loc > triggerHeight + 150) {
+        hdr.classList.add("scrolling");
+      } else {
+        hdr.classList.remove("scrolling");
+      }
+    });
+  }; // end ssMoveHeader
+
+  /* Mobile Menu
+   * ---------------------------------------------------- */
+  const ssMobileMenu = function () {
+    const toggleButton = document.querySelector(".s-header__menu-toggle");
+    const headerNavWrap = document.querySelector(".s-header__nav-wrap");
+    const siteBody = document.querySelector("body");
+
+    if (!(toggleButton && headerNavWrap)) return;
+
+    toggleButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleButton.classList.toggle("is-clicked");
+      siteBody.classList.toggle("menu-is-open");
+    });
+
+    headerNavWrap.querySelectorAll(".s-header__nav a").forEach(function (link) {
+      link.addEventListener("click", function (evt) {
+        // at 800px and below
+        if (window.matchMedia("(max-width: 800px)").matches) {
+          toggleButton.classList.toggle("is-clicked");
+          siteBody.classList.toggle("menu-is-open");
+        }
+      });
+    });
+
+    window.addEventListener("resize", function () {
+      // above 800px
+      if (window.matchMedia("(min-width: 801px)").matches) {
+        if (siteBody.classList.contains("menu-is-open"))
+          siteBody.classList.remove("menu-is-open");
+        if (toggleButton.classList.contains("is-clicked"))
+          toggleButton.classList.remove("is-clicked");
+      }
+    });
+  }; // end ssMobileMenu
+
+  /* Highlight active menu link on pagescroll
+   * ------------------------------------------------------ */
+  const ssScrollSpy = function () {
+    const sections = document.querySelectorAll(".target-section");
+
+    // Add an event listener listening for scroll
+    window.addEventListener("scroll", navHighlight);
+
+    function navHighlight() {
+      // Get current scroll position
+      let scrollY = window.pageYOffset;
+
+      // Loop through sections to get height(including padding and border),
+      // top and ID values for each
+      sections.forEach(function (current) {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        /* If our current scroll position enters the space where current section
+         * on screen is, add .current class to parent element(li) of the thecorresponding
+         * navigation link, else remove it. To know which link is active, we use
+         * sectionId variable we are getting while looping through sections as
+         * an selector
+         */
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document
+            .querySelector(".s-header__nav a[href*=" + sectionId + "]")
+            .parentNode.classList.add("current");
+        } else {
+          document
+            .querySelector(".s-header__nav a[href*=" + sectionId + "]")
+            .parentNode.classList.remove("current");
+        }
+      });
+    }
+  }; // end ssScrollSpy
+
+  /* Swiper
+   * ------------------------------------------------------ */
+  const ssSwiper = function () {
+    const mySwiper = new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        // when window width is >= 401px
+        401: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        // when window width is >= 801px
+        801: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      },
+    });
+  }; // end ssSwiper
+
+  /* Lightbox
+   * ------------------------------------------------------ */
+  const ssLightbox = function () {
+    const folioLinks = document.querySelectorAll(".folio-item a");
+    const modals = [];
+
+    folioLinks.forEach(function (link) {
+      let modalbox = link.getAttribute("href");
+      let instance = basicLightbox.create(document.querySelector(modalbox), {
+        onShow: function (instance) {
+          //detect Escape key press
+          document.addEventListener("keydown", function (evt) {
+            evt = evt || window.event;
+            if (evt.keyCode === 27) {
+              instance.close();
+            }
+          });
+        },
+      });
+      modals.push(instance);
+    });
+
+    folioLinks.forEach(function (link, index) {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        modals[index].show();
+      });
+    });
+  }; // end ssLightbox
+
+  /* Alert boxes
+   * ------------------------------------------------------ */
+  const ssAlertBoxes = function () {
+    const boxes = document.querySelectorAll(".alert-box");
+
+    boxes.forEach(function (box) {
+      box.addEventListener("click", function (e) {
+        if (e.target.matches(".alert-box__close")) {
+          e.stopPropagation();
+          e.target.parentElement.classList.add("hideit");
+
+          setTimeout(function () {
+            box.style.display = "none";
+          }, 500);
+        }
+      });
+    });
+  }; // end ssAlertBoxes
+
+  /* Smoothscroll
+   * ------------------------------------------------------ */
+  const ssSmoothScroll = function () {
+    const triggers = document.querySelectorAll(".smoothscroll");
+
+    triggers.forEach(function (trigger) {
+      trigger.addEventListener("click", function () {
+        const target = trigger.getAttribute("href");
+
+        Jump(target, {
+          duration: 1200,
+        });
+      });
+    });
+  }; // end ssSmoothScroll
+
+  /* back to top
+   * ------------------------------------------------------ */
+  const ssBackToTop = function () {
+    const pxShow = 900;
+    const goTopButton = document.querySelector(".ss-go-top");
+
+    if (!goTopButton) return;
+
+    // Show or hide the button
+    if (window.scrollY >= pxShow) goTopButton.classList.add("link-is-visible");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= pxShow) {
+        if (!goTopButton.classList.contains("link-is-visible"))
+          goTopButton.classList.add("link-is-visible");
+      } else {
+        goTopButton.classList.remove("link-is-visible");
+      }
+    });
+  }; // end ssBackToTop
+
+  /* initialize
+   * ------------------------------------------------------ */
+  (function ssInit() {
+    ssPreloader();
+    ssParallax();
+    ssMoveHeader();
+    ssMobileMenu();
+    ssScrollSpy();
+    ssSwiper();
+    ssLightbox();
+    ssAlertBoxes();
+    ssSmoothScroll();
+    ssBackToTop();
+  })();
+})(document.documentElement);
